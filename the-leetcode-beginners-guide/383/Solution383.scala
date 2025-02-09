31ff14d5b36e1998ca6c728aa6d7448de4083eb5
unibon4141 <vhcdyufg@gmail.com>
2025-02-09 17:44:01 +0900

    leetcode １問解いた

---
+++ the-leetcode-beginners-guide/383/Solution383.scala
@@ -0,0 +1,21 @@
+object Solution383 extends App {
+  def canConstruct(ransomNote: String, magazine: String): Boolean = {
+    def strTrim(ransomNote: String, ransomTmp: String, magazine: String, targetIndex: Int): Boolean = {
+      (ransomNote == ransomTmp, magazine.length) match {
+        case (true, _) => true
+        case (_, 0)    => false
+        case _ =>
+          val index = magazine.indexOf(ransomNote(targetIndex))
+          if (index >= 0) {
+            strTrim(ransomNote, ransomTmp :+ ransomNote(targetIndex), magazine.patch(index, "", 1), targetIndex + 1)
+          } else
+            false
+      }
+
+    }
+
+    strTrim(ransomNote, "", magazine, 0)
+
+  }
+  println(canConstruct("aa", "aab").toString())
+}
